b49befe8705b7d2081acae7133c262852aba9c38
Rindertanijk1997 <yhfe23.van.ri@folkuniversitetet.nu>
2023-12-20 19:37:32 +0100

    första

---
+++ script.js
@@ -0,0 +1,121 @@
+// G-Version - Split the Nota 
+
+// Skriv din pseudokod innanför nedanstående kommentarsblock 
+
+/* 
+
+godkänd men för enkel: 
+
+ 
+
+ 
+
+let totalSumma = 1000;  
+
+let antalVänner = 4;  
+
+let dricks = 0.10;  
+
+ 
+
+let totalDricks = totalSumma * dricks; 
+
+ 
+
+let nySumma = totalSumma + totalDricks;  
+
+ 
+
+let allaBetalar = nySumma / antalVänner; 
+
+ 
+
+console.log(allaBetalar); 
+
+ 
+
+ 
+
+ 
+
+/* försök till mer avenserad kod. 
+
+ 
+
+ 
+
+ANGE "totalsumma" OCH SPARA SOM totalSumma 
+
+ANGE "antal personer" OCH SPARA SOM antalVänner 
+
+ 
+
+OM totalSumma <= 1000 DÅ 
+
+   SÄTT dricks = 0.10 
+
+ANNARS 
+
+   SÄTT dricks = 0.15 
+
+SLUT OM 
+
+ 
+
+BERÄKNA totalDricks = totalSumma * dricks 
+
+BERÄKNA nySumma = totalSumma + totalDricks 
+
+BERÄKNA allaBetalar = nySumma / antalVänner 
+
+ 
+
+SKRIV UT allaBetalar 
+
+ 
+
+*/ 
+
+ 
+
+// VG-Version - Lewis Carroll Word Puzzle 
+
+// Skriv din pseudokod innanför nedanstående kommentarsblock 
+
+/* 
+
+ 
+
+function play() 
+
+    SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket 
+
+    SET variabel startOrd till "FOUR"; 
+
+    SET variabel slutordOrd till "FIVE"; 
+
+ 
+
+ 
+
+end function 
+
+ 
+
+function isOneLetterApart(wordOne, wordTwo) 
+
+    SET variabel diffCount till 0; 
+
+ 
+
+ 
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt 
+
+end function 
+
+ 
+
+*/ 
+
+ 
